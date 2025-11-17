b91e4ff521f46a325e9e7c6c498c050db49bb6ec
紫陽 水上 <shiyo.mizukami@treebell.co.jp>
2025-11-17 14:12:58 +0900

    新規作成

---
+++ src/main/java/textcreator/WebTextCreatorApplication.java
@@ -0,0 +1,13 @@
+package textcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTextCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTextCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/textcreator/WebTextCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package textcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTextCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
