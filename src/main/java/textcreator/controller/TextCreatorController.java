b960e34affbbfe31a01eef871d506d70d2ad75ec
紫陽 水上 <shiyo.mizukami@treebell.co.jp>
2025-11-18 14:43:52 +0900

    main.html作成 application.properties更新

--- src/main/java/textcreator/controller/TextCreatorController.java
+++ src/main/java/textcreator/controller/TextCreatorController.java
@@ -1,8 +1,13 @@
 package textcreator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TextCreatorController {
 
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
 }
